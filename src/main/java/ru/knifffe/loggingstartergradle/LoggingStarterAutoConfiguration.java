7843257452851a6ffbfbf69a465b8f6fefe45b11
itsme <knifffe@yandex.ru>
2025-03-10 21:55:31 +0300

    add weblfilter

--- src/main/java/ru/knifffe/loggingstartergradle/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/knifffe/loggingstartergradle/LoggingStarterAutoConfiguration.java
@@ -3,6 +3,8 @@ package ru.knifffe.loggingstartergradle;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import ru.knifffe.loggingstartergradle.aspect.LogExecutionAspect;
+import ru.knifffe.loggingstartergradle.webfilter.WebLoggingFilter;
+import ru.knifffe.loggingstartergradle.webfilter.WebLoggingRequestBodyAdvice;
 
 @AutoConfiguration
 public class LoggingStarterAutoConfiguration {
@@ -10,5 +12,15 @@ public class LoggingStarterAutoConfiguration {
     @Bean
     public LogExecutionAspect logExecutionAspect() {
         return new LogExecutionAspect();
+    }
+
+    @Bean
+    public WebLoggingFilter webLoggingFilter() {
+        return new WebLoggingFilter();
+    }
+
+    @Bean
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
+        return new WebLoggingRequestBodyAdvice();
     }
 }
