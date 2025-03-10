b603cc7d688f353cc24a1ff1305266642bd22391
itsme <knifffe@yandex.ru>
2025-03-10 22:20:52 +0300

    add weblfilter

--- src/main/java/ru/knifffe/loggingstartergradle/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/knifffe/loggingstartergradle/LoggingStarterAutoConfiguration.java
@@ -1,25 +1,33 @@
 package ru.knifffe.loggingstartergradle;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import ru.knifffe.loggingstartergradle.aspect.LogExecutionAspect;
 import ru.knifffe.loggingstartergradle.webfilter.WebLoggingFilter;
 import ru.knifffe.loggingstartergradle.webfilter.WebLoggingRequestBodyAdvice;
 
 @AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
+
     public LogExecutionAspect logExecutionAspect() {
         return new LogExecutionAspect();
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true", matchIfMissing = true)
+
     public WebLoggingFilter webLoggingFilter() {
         return new WebLoggingFilter();
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = {"enabled", "log-body"}, havingValue = "true")
+
     public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
         return new WebLoggingRequestBodyAdvice();
     }
